7bc679b7d16eda1bc786a6aabfebfb0c99653f0a
Yanbo.wang <yanbo.wang@yitu-inc.intra>
2020-01-16 15:32:02 +0800

    tst

--- Test.py
+++ Test.py
@@ -10,35 +10,11 @@ from TimeNormalizer import TimeNormalizer # 引入包
 
 tn = TimeNormalizer()
 
-res = tn.parse(target=u'晚上8点到上午10点之间') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
+while(1):
+    query = input('Input:')
+    res = tn.parse(target=query) # target为待分析语句，timeBase为基准时间默认是当前时间
+    print(res)
 
-res = tn.parse(target=u'2013年二月二十八日下午四点三十分二十九秒', timeBase='2013-02-28 16:30:29') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
-
-res = tn.parse(target=u'我需要大概33天2分钟四秒', timeBase='2013-02-28 16:30:29') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
-
-res = tn.parse(target=u'今年儿童节晚上九点一刻') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
-
-res = tn.parse(target=u'三日') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
-
-res = tn.parse(target=u'7点4') # target为待分析语句，timeBase为基准时间默认是当前时间
-print(res)
-
-res = tn.parse(target=u'今年春分')
-print(res)
-
-res = tn.parse(target=u'7000万')
-print(res)
-
-res = tn.parse(target=u'7百')
-print(res)
-
-res = tn.parse(target=u'7千')
-print(res)
 
 #
 #
